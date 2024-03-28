0a7805d2ed1f263f2fefa57005ec8bea956ae22a
SammyJoseph <sam.tab.paz@gmail.com>
2024-03-28 16:59:17 -0500

    clase #2 - 3:29

--- database/migrations/2024_03_28_212218_create_products_table.php
+++ database/migrations/2024_03_28_212218_create_products_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->title('title');
+            $table->string('image');
+            $table->float('price');
             $table->timestamps();
         });
     }
